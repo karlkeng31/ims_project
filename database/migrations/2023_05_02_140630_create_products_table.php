6ac1b5cb50c77ab7ee7fcb12845fc35af26532f6
Karl Ken. G. <formycodingonly@gmail.com>
2024-01-04 00:54:27 +0800

    add new table

--- database/migrations/2023_05_02_140630_create_products_table.php
+++ database/migrations/2023_05_02_140630_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('slug');
             $table->string('code')->unique()->nullable();
@@ -24,15 +25,18 @@ return new class extends Migration
             $table->integer('tax')->nullable();
             $table->tinyInteger('tax_type')->nullable();
             $table->text('notes')->nullable();
+
             $table->string('product_image')->nullable();
-            /*  $table->foreignIdFor(\App\Models\Category::class)
+
+            $table->foreignIdFor(\App\Models\Category::class)
                 ->nullable()
                 ->constrained()
-                ->restrictOnDelete()
-                ->cascadeOnDelete()
+//                ->restrictOnDelete();
+//                ->cascadeOnDelete();
                 ->nullOnDelete();
+
             $table->foreignIdFor(\App\Models\Unit::class)->constrained()
-                ->cascadeOnDelete(); */
+                ->cascadeOnDelete();
             $table->timestamps();
         });
     }
