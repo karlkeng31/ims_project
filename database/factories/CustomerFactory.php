29a2da59a9268f180771d60b10393f9a50e449ea
Karl Ken. G. <formycodingonly@gmail.com>
2024-01-05 17:20:59 +0800

    factories added

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
+            'address' => fake()->address(),
+            'photo' => fake()->image(),
+            'account_holder' => fake()->randomNumber(),
+            'account_number' => fake()->randomNumber(),
+            'bank_name' => fake()->name(),
+        ];
+    }
+}
---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Supplier>
+ */
+class SupplierFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
+            'address' => fake()->streetAddress(),
+            'shopname' => fake()->company(),
+            /* 'type' => fake()->randomNumber(1, 5), */
+            'photo' => fake()->image(),
+            'account_holder' => fake()->randomNumber(5),
+            'account_number' => fake()->randomNumber(5),
+            'bank_name' => fake()->randomNumber(5),
+        ];
+    }
+}
